f23693c4d6c21320d84c1d645f91316bf69fcb0f
ray <me@rayhanadev.com>
2024-12-15 21:15:45 -0600

    fix: hotfix to make rss work

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -1,34 +1,38 @@
 import type { APIRoute } from "astro";
-import { getCollection, render } from "astro:content";
+import { getCollection } from "astro:content";
 
 import rss from "@astrojs/rss";
 
 import { BLOG_TITLE, BLOG_DESCRIPTION, EMAIL_ADDRESS } from "lib/consts";
 
 export const GET: APIRoute = async () => {
-  const blog = await getCollection("blog");
-  return rss({
-    title: BLOG_TITLE,
-    description: BLOG_DESCRIPTION,
-    site: import.meta.env.SITE,
-    items: await Promise.all(
-      blog.map(async (post) => {
-        const { remarkPluginFrontmatter } = await render(post);
-        return {
-          title: post.data.title,
-          description: post.data.description,
-          link: `/blog/${post.id}/`,
-          pubDate: remarkPluginFrontmatter.lastModified,
-          language: "en-us",
-          copyright: `Copyright © ${new Date().getUTCFullYear()}, Rayhan Noufal Arayilakath`,
-          author: EMAIL_ADDRESS,
-          managingEditor: EMAIL_ADDRESS,
-          webMaster: EMAIL_ADDRESS,
-          docs: "https://cyber.harvard.edu/rss/rss.html",
-          ttl: 60,
-          lastBuildDate: new Date(),
-        };
-      }),
-    ),
-  });
+    const blog = await getCollection("blog");
+    return rss({
+        title: BLOG_TITLE,
+        description: BLOG_DESCRIPTION,
+        site: import.meta.env.SITE,
+        items: await Promise.all(
+            blog.map(async (post) => {
+                const [y, m, d] =
+                    post.filePath!.split("/")[4]?.split("-") ?? [];
+
+                const date = new Date(`${y}-${m}-${d}`);
+
+                return {
+                    title: post.data.title,
+                    description: post.data.description,
+                    link: `/blog/${post.id}/`,
+                    pubDate: date,
+                    language: "en-us",
+                    copyright: `Copyright © ${new Date().getUTCFullYear()}, Rayhan Noufal Arayilakath`,
+                    author: EMAIL_ADDRESS,
+                    managingEditor: EMAIL_ADDRESS,
+                    webMaster: EMAIL_ADDRESS,
+                    docs: "https://www.rssboard.org/rss-specification",
+                    ttl: 60,
+                    lastBuildDate: new Date(),
+                };
+            }),
+        ),
+    });
 };
