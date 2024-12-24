fd94f1dfc56b919e19722a088e28026ce006aaa9
ray <me@rayhanadev.com>
2024-12-24 00:58:39 -0600

    feat: setup obsidian vault in repo

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,7 +4,7 @@ import { glob } from "astro/loaders";
 const blog = defineCollection({
     loader: glob({
         pattern: import.meta.env.DEV ? "**/*.{md,mdx}" : "**/[^_]*.{md,mdx}",
-        base: "./src/content/blog",
+        base: "./src/content/blog/posts",
     }),
     schema: z.object({
         title: z.string(),
