c202d821ee448580186bcb6de4404702f805009c
ray <me@rayhanadev.com>
2025-01-01 21:15:07 -0600

    feat: replace ga with umami

--- env.ts
+++ env.ts
@@ -1,7 +1,7 @@
 import { envField as e } from "astro/config";
 
 export const schema = {
-    PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID: e.string({
+    PUBLIC_UMAMI_WEBSITE_ID: e.string({
         context: "client",
         access: "public",
         optional: true,
